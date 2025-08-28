c7f3d8cb0905aa859228217f54daaf53d59004f6
YoranHansel <yoranhansel@gmail.com>
2025-08-28 05:58:59 -0300

    Att3

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let botao = document.querySelectorAll(".botao button")
+const descricoes = [
+    "A imagem é uma foto real do fenômeno da aurora boreal",
+    "Nesta imagem podemos ver os animais caindo do céu como uma chuva e uma garota se protegendo com o guarda chuva.",
+    "Na imagem mostra o relâmpago eterno do catatumbo, como podemos ver, há muitos raios caindo na região ao mesmo tempo.",
+    "Aqui podemos observar a maré vermelha, onde a água está com a cor avermelhada.",
+    "A imagem mostra uma água viva de regiões profundas brilhando, mostrando a bioluminescência."
+];
 
-
-botao.addEventListener("click", function(){
-  
-    alert("A imagem mostra o fenômeno da aurora boreal")
-
-})
+const botoes = document.querySelectorAll(".botao");
+ 
+botoes.forEach((botao, index) => {
+    botao.addEventListener("click", () => {
+        alert(descricoes[index]);
+    });
+});
