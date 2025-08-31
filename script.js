5545a3f2735da8c2208579c96dd7dc202be4ee50
YoranHansel <yoranhansel@gmail.com>
2025-08-31 19:52:40 -0300

    Att4

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const descricoes = [
     "A imagem é uma foto real do fenômeno da aurora boreal",
-    "Nesta imagem podemos ver os animais caindo do céu como uma chuva e uma garota se protegendo com o guarda chuva.",
-    "Na imagem mostra o relâmpago eterno do catatumbo, como podemos ver, há muitos raios caindo na região ao mesmo tempo.",
+    "Nesta imagem podemos ver os animais caindo do céu como uma chuva e uma garota se protegendo com o guarda-chuva.",
+    "Na imagem mostra o relâmpago eterno do Catatumbo, como podemos ver, há muitos raios caindo na região ao mesmo tempo.",
     "Aqui podemos observar a maré vermelha, onde a água está com a cor avermelhada.",
-    "A imagem mostra uma água viva de regiões profundas brilhando, mostrando a bioluminescência."
+    "A imagem mostra uma água-viva de regiões profundas brilhando, mostrando a bioluminescência."
 ];
 
-const botoes = document.querySelectorAll(".botao");
- 
-botoes.forEach((botao, index) => {
-    botao.addEventListener("click", () => {
-        alert(descricoes[index]);
-    });
-});
+
+document.getElementById("botao1").addEventListener("click", () => alert(descricoes[0]));
+document.getElementById("botao2").addEventListener("click", () => alert(descricoes[1]));
+document.getElementById("botao3").addEventListener("click", () => alert(descricoes[2]));
+document.getElementById("botao4").addEventListener("click", () => alert(descricoes[3]));
+document.getElementById("botao5").addEventListener("click", () => alert(descricoes[4]));
